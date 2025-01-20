988b14670ff710215cfffa31df370d0fbc8c3ac0
rainbowchuckles <63379174+rainbowchuckles@users.noreply.github.com>
2025-01-20 08:49:49 +0000

    Deploying to gh-pages from @ rainbowchuckles/rainbowchuckles.github.io@836585775994f3ef2e5499f31936bdebe47d60e0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,18 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-post-phd-update-new-website",
-      
-        title: "post-phd update! new website!",
-      
-      description: "starting the post-doc and launching the website",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2025/update/";
-        
-      },
-    },{id: "post-arse-article-on-us",
+        },{id: "post-arse-article-on-us",
       
         title: 'arse article on us... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
       
