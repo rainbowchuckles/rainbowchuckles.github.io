4ccd2301ee22d7466313fddfa79fbe8d7e2d8f3f
rainbowchuckles <63379174+rainbowchuckles@users.noreply.github.com>
2025-01-22 16:36:25 +0000

    Deploying to gh-pages from @ rainbowchuckles/rainbowchuckles.github.io@3d3ab845442cdd224c94de578c8ba973179bdb37 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "This is an abbreviated version of my CV, please download the full version by clicking the icon on the right.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
